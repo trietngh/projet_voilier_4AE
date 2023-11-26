50770b0b6f6cc16cea2b978a56dcbac7e650d368
trietngh <hoangtriet26@gmail.com>
2023-11-26 23:20:27 +0100

    Negligeable change on principal.c but commit just in case

--- Principal.c
+++ Principal.c
@@ -12,10 +12,10 @@ int main(void){
 	PLATEAU_Enable();
 	
 
-	RTC_Init();
-	RTC_Enable();
-	RTC_SetTime(1,18,9,22,15,10,17); // date, jour, mois, annee, heure, minute, seconde
-	RTC_GetTime(); 
+	//RTC_Init();
+	//RTC_Enable();
+	//RTC_SetTime(1,18,9,22,15,10,17); // date, jour, mois, annee, heure, minute, seconde
+	//RTC_GetTime(); 
 	do
 	{
 		
